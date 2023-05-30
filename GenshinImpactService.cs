3113afc740bc9732eb42fc66820c6412a5757276
Alexander <satan1c@ukr.net>
2023-05-30 23:06:23 +0300

    format fix

--- GenshinImpactService.cs
+++ GenshinImpactService.cs
@@ -1,7 +1,5 @@
-﻿using HoYoLabApi.Classes;
-using HoYoLabApi.Enums;
+﻿using HoYoLabApi.Enums;
 using HoYoLabApi.interfaces;
-using HoYoLabApi.Models;
 using HoYoLabApi.Static;
 
 namespace HoYoLabApi.GenshinImpact;
@@ -11,38 +9,50 @@ public class GenshinImpactService : GenshinImpactServiceBase
 	public GenshinImpactService(IHoYoLabClient client) : base(client)
 	{
 	}
-	
+
 	public async Task DailiesClaimAsync(ICookies[] cookies)
 	{
 		await foreach (var _ in DailiesClaimAsync(cookies, null))
 		{
 		}
 	}
-	
+
 	public async Task CodesClaimAsync(ICookies cookies, string[] codes)
 	{
 		await foreach (var _ in CodesClaimAsync(cookies, codes, null))
 		{
 		}
 	}
-	
+
 	public Task<IDailyClaimResult> DailyClaimAsync(string cookies)
-		=> base.DailyClaimAsync(cookies.ParseCookies());
-	
+	{
+		return base.DailyClaimAsync(cookies.ParseCookies());
+	}
+
 	public Task<IDailyClaimResult> DailyClaimAsync()
-		=> base.DailyClaimAsync(Client.Cookies!);
+	{
+		return base.DailyClaimAsync(Client.Cookies!);
+	}
 
 	public Task<ICodeClaimResult> CodeClaimAsync(string code)
-		=> base.CodeClaimAsync(Client.Cookies!, code);
-	
+	{
+		return base.CodeClaimAsync(Client.Cookies!, code);
+	}
+
 	public IAsyncEnumerable<IDailyClaimResult> DailiesClaimAsync(string[] cookies, CancellationToken? cancellationToken)
-		=> base.DailiesClaimAsync(cookies.Select(x => x.ParseCookies()).ToArray(), cancellationToken);
+	{
+		return base.DailiesClaimAsync(cookies.Select(x => x.ParseCookies()).ToArray(), cancellationToken);
+	}
 
 	public Task CodesClaimAsync(string cookies, string[] codes)
-		=> CodesClaimAsync(cookies.ParseCookies(), codes);
+	{
+		return CodesClaimAsync(cookies.ParseCookies(), codes);
+	}
 
 	public Task DailiesClaimAsync(string[] cookies)
-		=> DailiesClaimAsync(cookies.Select(x => x.ParseCookies()).ToArray());
+	{
+		return DailiesClaimAsync(cookies.Select(x => x.ParseCookies()).ToArray());
+	}
 
 	public IAsyncEnumerable<ICodeClaimResult> CodesClaimAsync(
 		string[] codes,
