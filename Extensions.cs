14d69f1033c3313d92928787875507b15d22ba42
Alexander <satan1c@ukr.net>
2023-05-30 16:10:58 +0300

    little rewrite

--- Extensions.cs
+++ Extensions.cs
@@ -1,19 +1,9 @@
 ﻿using HoYoLabApi.Enums;
 
-namespace HoyoLabApi.GenshinImpact;
+namespace HoYoLabApi.GenshinImpact;
 
 public static class Extensions
 {
-	public static string GetGenshinLang(this Language language)
-	{
-		return language switch
-		{
-			Language.English => "en",
-			Language.Russian => "ru",
-			_ => "en"
-		};
-	}
-	
 	public static string GetGenshinRegion(this Region region)
 	{
 		return region switch
